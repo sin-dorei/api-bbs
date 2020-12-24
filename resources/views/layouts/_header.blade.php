d82f5f9378dec9c1cc1e43ccb757fb7e4a650de2
sin-dorei <228827120@qq.com>
2020-12-24 09:51:25 +0000

    集成 administrator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,61 +1,80 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
   <div class="container">
-      <!-- Branding Image -->
-      <a class="navbar-brand " href="{{ url('/') }}">
+    <!-- Branding Image -->
+    <a class="navbar-brand " href="{{ url('/') }}">
       LaraBBS
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-      </button>
+    </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item @if(request()->url() === route('topics.index')) active @endif"><a class="nav-link" href="{{ route('topics.index') }}">话题</a></li>
-        <li class="nav-item @if(request()->url() === route('categories.show', 1)) active @endif"><a class="nav-link" href="{{ route('categories.show', 1) }}">分享</a></li>
-        <li class="nav-item @if(request()->url() === route('categories.show', 2)) active @endif"><a class="nav-link" href="{{ route('categories.show', 2) }}">教程</a></li>
-        <li class="nav-item @if(request()->url() === route('categories.show', 3)) active @endif"><a class="nav-link" href="{{ route('categories.show', 3) }}">问答</a></li>
-        <li class="nav-item @if(request()->url() === route('categories.show', 4)) active @endif"><a class="nav-link" href="{{ route('categories.show', 4) }}">公告</a></li>
+        <li class="nav-item @if(request()->url() === route('topics.index')) active @endif"><a class="nav-link"
+            href="{{ route('topics.index') }}">话题</a></li>
+        <li class="nav-item @if(request()->url() === route('categories.show', 1)) active @endif"><a class="nav-link"
+            href="{{ route('categories.show', 1) }}">分享</a></li>
+        <li class="nav-item @if(request()->url() === route('categories.show', 2)) active @endif"><a class="nav-link"
+            href="{{ route('categories.show', 2) }}">教程</a></li>
+        <li class="nav-item @if(request()->url() === route('categories.show', 3)) active @endif"><a class="nav-link"
+            href="{{ route('categories.show', 3) }}">问答</a></li>
+        <li class="nav-item @if(request()->url() === route('categories.show', 4)) active @endif"><a class="nav-link"
+            href="{{ route('categories.show', 4) }}">公告</a></li>
       </ul>
 
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
-          <!-- Authentication Links -->
-          @guest
-          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
-          @else
-          <li class="nav-item">
-            <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
-              <i class="fa fa-plus"></i>
-            </a>
-          </li>
-          <li class="nav-item notification-badge">
-            <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white" href="{{ route('notifications.index') }}">
-              {{ Auth::user()->notification_count }}
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
-              {{ Auth::user()->name }}
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}"><i class="far fa-user mr-2"></i>个人中心</a>
-              <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}"><i class="far fa-edit mr-2"></i>编辑资料</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" id="logout" href="#">
-                  <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
+        <!-- Authentication Links -->
+        @guest
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        @else
+        <li class="nav-item">
+          <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+            <i class="fa fa-plus"></i>
+          </a>
+        </li>
+        <li class="nav-item notification-badge">
+          <a class="nav-link mr-3 badge badge-pill badge-{{ Auth::user()->notification_count > 0 ? 'hint' : 'secondary' }} text-white"
+            href="{{ route('notifications.index') }}">
+            {{ Auth::user()->notification_count }}
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
 
-                  @csrf
+            @can('manage_contents')
+            <a class="dropdown-item" href="{{ url(config('administrator.uri')) }}">
+              <i class="fas fa-tachometer-alt mr-2"></i>
+              管理后台
+            </a>
+            <div class="dropdown-divider"></div>
+            @endcan
 
-                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
-                  </form>
-              </a>
-              </div>
-          </li>
-          @endguest
+            <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}"><i
+                class="far fa-user mr-2"></i>个人中心</a>
+            <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}"><i
+                class="far fa-edit mr-2"></i>编辑资料</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" id="logout" href="#">
+              <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
+
+                @csrf
+
+                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+              </form>
+            </a>
+          </div>
+        </li>
+        @endguest
       </ul>
-      </div>
+    </div>
   </div>
-  </nav>
+</nav>
