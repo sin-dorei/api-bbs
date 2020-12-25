3c76e4ef36c200a4c728aadf95246cba08738d5d
sin-dorei <228827120@qq.com>
2020-12-25 07:14:55 +0000

    注册用户默认头像

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Topic;
-use App\Observers\TopicObserver;
-use App\Models\Reply;
-use App\Observers\ReplyObserver;
-use App\Models\Link;
-use App\Observers\LinkObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,8 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 	{
-        Topic::observe(TopicObserver::class);
-        Reply::observe(ReplyObserver::class);
-        Link::observe(LinkObserver::class);
+        \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
     }
 }
